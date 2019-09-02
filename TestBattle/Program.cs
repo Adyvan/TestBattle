804386945d49f9c03e7bba5d87fa20be835b4866
Vladimir Adushkevich <vladimir.adushkevich@gmail.com>
2019-09-02 13:18:58 +0300

    Init commit

---
+++ TestBattle/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestBattle
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
